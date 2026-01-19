08efdb747ed388da072429a578e4c2ba14da4dd6
LeeGiSu <soo@marketboro.com>
2026-01-19 17:24:54 +0900

    searchConsole, adsense 제거

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,15 +4,4 @@
  * See: https://www.gatsbyjs.com/docs/ssr-apis/
  */
 
-import React from "react"
-
-export const onRenderBody = ({ setHeadComponents }) => {
-  setHeadComponents([
-    <script
-      key="adsbygoogle"
-      async
-      src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1472895926659868"
-      crossOrigin="anonymous"
-    />,
-  ])
-}
+// Analytics/ads scripts removed.
